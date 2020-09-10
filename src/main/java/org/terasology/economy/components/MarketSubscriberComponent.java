7c5e1ea45316f2b204748384572070ba1ab2a4ec
DarkWeird <foxflameless@gmail.com>
2020-09-10 13:39:33 +0300

    feature(reintegrate-gestaltv7):  move reflection package to nui package.

--- src/main/java/org/terasology/economy/components/MarketSubscriberComponent.java
+++ src/main/java/org/terasology/economy/components/MarketSubscriberComponent.java
@@ -6,7 +6,7 @@ package org.terasology.economy.components;
 
 import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.reflection.MappedContainer;
+import org.terasology.nui.reflection.MappedContainer;
 
 import java.util.HashMap;
 import java.util.Map;
