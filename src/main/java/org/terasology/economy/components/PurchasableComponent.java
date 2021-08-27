f726b325e98be2c8df720b5b9c6e4abee98f9e5c
Harshit <av.harshith15@gmail.com>
2021-08-27 20:39:21 +0530

    update

--- src/main/java/org/terasology/economy/components/PurchasableComponent.java
+++ src/main/java/org/terasology/economy/components/PurchasableComponent.java
@@ -2,12 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.economy.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.world.block.items.AddToBlockBasedItem;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Indicates that the item can be bought and thus will be available in the shop
  */
 @AddToBlockBasedItem
-public class PurchasableComponent implements Component {
+public class PurchasableComponent implements Component<PurchasableComponent> {
+    @Override
+    public void copyFrom(PurchasableComponent other) {
+    }
 }
--- src/main/java/org/terasology/economy/components/ValueComponent.java
+++ src/main/java/org/terasology/economy/components/ValueComponent.java
@@ -2,17 +2,22 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.economy.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.world.block.items.AddToBlockBasedItem;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Indicates that the given entity has a value.
  */
 @AddToBlockBasedItem
-public class ValueComponent implements Component {
+public class ValueComponent implements Component<ValueComponent> {
 
     /**
      * How much money one instance of this entity is worth
      */
     public int value;
+
+    @Override
+    public void copyFrom(ValueComponent other) {
+        this.value = other.value;
+    }
 }
