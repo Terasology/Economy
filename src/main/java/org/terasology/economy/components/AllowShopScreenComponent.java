94e195283632cb8bc14deb0d622a77f72c1025a2
Tobias Nett <skaldarnar@googlemail.com>
2021-12-04 00:41:34 +0100

    feature: make shop screen optional
    Make the shop screen displayed with the inventory optional.
    
    This is done by introducing a new marker component `AllowShopScreenComponent` and a high-priority event handler to open a different screen if this component is present on a client.
    
    The README contains a section explaining how the screen can be enabled.

---
+++ src/main/java/org/terasology/economy/components/AllowShopScreenComponent.java
@@ -0,0 +1,12 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.economy.components;
+
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
+
+/**
+ * A marker component to denote that the respective client should see an alternative inventory screen with in-game shop.
+ */
+public class AllowShopScreenComponent extends EmptyComponent<AllowShopScreenComponent> {
+}
