4548d2cf85e00ec9816438856482120348ed76d3
Tobias Nett <skaldarnar@googlemail.com>
2021-12-04 00:46:40 +0100

    fixup! feature: make shop screen optional

--- src/main/java/org/terasology/economy/ui/MarketUiClientSystem.java
+++ src/main/java/org/terasology/economy/ui/MarketUiClientSystem.java
@@ -3,8 +3,6 @@
 
 package org.terasology.economy.ui;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.economy.components.AllowShopScreenComponent;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.event.EventPriority;
@@ -21,25 +19,22 @@ import org.terasology.module.inventory.input.InventoryButton;
 @RegisterSystem(RegisterMode.CLIENT)
 public class MarketUiClientSystem extends BaseComponentSystem {
 
-    Logger logger = LoggerFactory.getLogger(MarketUiClientSystem.class);
-
     @In
     private NUIManager nuiManager;
 
     /**
      * Show an alternative to the {@link org.terasology.module.inventory.ui.InventoryScreen InventoryScreen} with an additional in-game shop
      * if the client also has the {@link AllowShopScreenComponent}.
-     *
+     * <p>
      * This event handler is registered with a higher priority than
-     * {@link org.terasology.module.inventory.systems.InventoryUIClientSystem#onToggleInventory(InventoryButton, EntityRef)} to open a
-     * different screen in case the respective marker component is present.
+     * {@link org.terasology.module.inventory.systems.InventoryUIClientSystem#onToggleInventory(InventoryButton,
+     * EntityRef)} to open a different screen in case the respective marker component is present.
      *
      * @param event the input event to open the inventory screen
      * @param entity the client entity requesting to open the inventory screen
      */
-    @ReceiveEvent(components = {ClientComponent.class,  AllowShopScreenComponent.class}, priority = EventPriority.PRIORITY_HIGH)
+    @ReceiveEvent(components = {ClientComponent.class, AllowShopScreenComponent.class}, priority = EventPriority.PRIORITY_HIGH)
     public void onToggleInventory(InventoryButton event, EntityRef entity) {
-        logger.info(entity.toFullDescription());
         if (event.getState() == ButtonState.DOWN) {
             nuiManager.toggleScreen("Economy:shopScreen");
             event.consume();
