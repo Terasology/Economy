2a836c8c1b340c22826d2c92297bab31969c30d3
thierrywong <wong.t@sfeir.com>
2023-12-15 23:31:31 +0100

    Refactor Components without attributes to EmptyComponent

--- src/main/java/org/terasology/economy/components/MultiInvStorageComponent.java
+++ src/main/java/org/terasology/economy/components/MultiInvStorageComponent.java
@@ -4,18 +4,13 @@ package org.terasology.economy.components;
 
 import com.google.common.collect.Lists;
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 import java.util.List;
 
 /**
  * This stores references to chests for a building entity. They are distinguished into chests to draw resources from and chests to store into.
  */
-public class MultiInvStorageComponent implements Component<MultiInvStorageComponent> {
+public class MultiInvStorageComponent extends EmptyComponent<MultiInvStorageComponent> {
     public List<EntityRef> chests = Lists.newArrayList();
-
-    @Override
-    public void copyFrom(MultiInvStorageComponent other) {
-
-    }
 }
